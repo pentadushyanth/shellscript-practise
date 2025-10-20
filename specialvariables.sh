51ba114c09f8e836f285602f1d2cbc039afe00d4
dushyanth <pentadushyanth@gmail.com>
2025-10-20 13:00:12 +0530

    specialvariables

--- specialvariables.sh
+++ specialvariables.sh
@@ -2,3 +2,10 @@
 
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
+echo "scriptname:$0"
+echo "current directory: $PWD"
+echo " who is running this: $USER"
+echo " Home directory of user: $HOME"
+echo "PID of this script: $$"
+sleep 50 &
+echo "PID of the last command in the background is: $!"
