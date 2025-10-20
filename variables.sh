28beeb9245d49995a7c25c60633729dbf9e41a93
dushyanth <pentadushyanth@gmail.com>
2025-10-20 11:36:52 +0530

    variables_updated

--- variables.sh
+++ variables.sh
@@ -3,5 +3,5 @@
 person1=john
 person2=Raheem
 
-echo"$person1:: Hi $person2,How are you?"
-echo"$person2:: Hi $person1,How are you?"
+echo "$person1:: Hi $person2,How are you?"
+echo "$person2:: Hi $person1,How are you?"
