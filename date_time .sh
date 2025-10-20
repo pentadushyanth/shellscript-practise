c6bc3e58ffbb189a616b1c3d3c305bcb4a85ebc8
dushyanth <pentadushyanth@gmail.com>
2025-10-20 12:44:59 +0530

    date and time

--- date_time .sh
+++ date_time .sh
@@ -1,5 +1,11 @@
 #! /bin/bash/
 
-Date=$(date)
+#Date=$(date)
 
-echo "time stamp executed : $Date"
+start_time=$(date +%s)
+
+sleep 10 
+
+end_time=$(date +%s)
+Total_time=$(($end_time-$start_time))
+echo "script executed in  : $Total_time seconds" 
