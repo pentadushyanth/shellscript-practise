b4fef26f7eefe5baf9c75847723c03b91c7c2ae6
dushyanth <pentadushyanth@gmail.com>
2025-10-21 18:11:00 +0530

    installations

--- installation.sh
+++ installation.sh
@@ -4,14 +4,14 @@ Userid=$(id -u)
 
 if [ $Userid -ne 0 ]; then
     echo "Error:: Please run this script with root privilige"
-
+    exit 1 #failure is pther than 0
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo " Error:: Installing mysql is failure"
-
+    exit 2
 else
     echo " installing mysql is success"
 fi
