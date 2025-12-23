277416213af67dd611c5c119315ce2f91a00b396
dushyanth <pentadushyanth@gmail.com>
2025-12-23 11:56:19 +0530

    first commit

--- Roboshop/roboshop.sh
+++ Roboshop/roboshop.sh
@@ -5,7 +5,7 @@ sg_id="sg-055d475babe24887e"
 
 for instance  in $@
 do 
-    Instance_ID=$(aws ec2 run-instances --image-id $ami_id  --instance-type t2.micro  \
+    Instance_ID=$(aws ec2 run-instances --image-id $ami_id  --instance-type t3.micro  \
     --security-group-ids $sg_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' --output text)
   if [ $instance != "frontend" ]; then
