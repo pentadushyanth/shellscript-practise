d5ff4cb9a290c80e3cda91a6ace02d9f274f8cea
dushyanth <pentadushyanth@gmail.com>
2025-12-26 10:24:41 +0530

    first commit

--- Roboshop/roboshop.sh
+++ Roboshop/roboshop.sh
@@ -2,7 +2,8 @@
 
 ami_id="ami-09c813fb71547fc4f"
 sg_id="sg-055d475babe24887e"
-
+zone_id="Z00418011PRHOYGK0K56N"
+domain_name="practisedevops.shop"
 for instance  in $@
 do 
     Instance_ID=$(aws ec2 run-instances --image-id $ami_id  --instance-type t3.micro  \
@@ -13,13 +14,33 @@ do
     --instance-ids $Instance_ID  \
     --query 'Reservations[0].Instances[0].PrivateIpAddress' \
     --output text)
+    Record_name="$instance.$domain_name"
   else
      IP=$(aws ec2 describe-instances \
     --instance-ids $Instance_ID  \
     --query 'Reservations[0].Instances[0].PublicIpAddress' \
     --output text)
+    Record_name="$domain_name"
   fi
 
   echo "$instance instance $IP"
-done
  
+ aws route53 change-resource-record-sets \
+  --hosted-zone-id $zone_id \
+  --change-batch '
+  {
+    "Comment": "updating record set"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$Record_name'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "' $IP '"
+        }]
+      }
+    }]
+  }
+  '
+done
