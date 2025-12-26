7dfae8e4f9d57f93dea147b1826c2284275ff3a2
dushyanth <pentadushyanth@gmail.com>
2025-12-26 22:10:14 +0530

    first commit

---
+++ Roboshop/mysql.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+Start_time=$(date +%s)
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e " $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? " mysql installed"
+
+systemctl enable mysqld &>>$LOG_FILE
+systemctl start mysqld  &>>$LOG_FILE
+VALIDATE $? "mysql started"
+
+mysql_secure_installation --set-root-pass RoboShop@1 
+VALIDATE $? "setting root password"
+
+End_Time=$(date +%s)
+total_time=$(( $End_Time - $Start_time))
+echo -e "Script executed in: $Y $total_time seconds $N"
