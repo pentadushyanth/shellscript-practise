8d4a046ba76023e86d400cda88d2d38955a849f2
dushyanth <pentadushyanth@gmail.com>
2025-12-30 17:44:30 +0530

    second commit

---
+++ Roboshop/payment.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+script_dir=$PWD
+mongodb_host=mongodb.practisedevops.shop
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+Mysql_Host=mysql.practisedevops.shop
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e " $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf install python3 gcc python3-devel -y &>>$LOG_FILE
+VALIDATE $? "Python installation"
+
+id roboshop
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "creating system user"
+else 
+    echo -e "user already exists...$Y SKIPPING $N"
+fi
+
+mkdir -p /app 
+VALIDATE $? "creating directory"
+
+curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOG_FILE
+VALIDATE $? "downloading payment"
+cd /app 
+VALIDATE $? "change directory"
+
+rm -rf /app/*
+VALIDATE $? "removing existing code"
+
+unzip /tmp/payment.zip &>>$LOG_FILE
+VALIDATE $? "unzipping file"
+
+
+cd /app 
+VALIDATE $? "Chnaging directory"
+
+pip3 install -r requirements.txt &>>$LOG_FILE
+VALIDATE $? "requirements installation"
+
+cp $script_dir/payment.service /etc/systemd/system/payment.service
+VALIDATE $? "copying service"
+
+systemctl daemon-reload
+VALIDATE $? "daemon reload"
+
+systemctl enable payment &>>$LOG_FILE
+VALIDATE $? "payment enabled"
+
+systemctl start payment &>>$LOG_FILE
+VALIDATE $? "payment started"
