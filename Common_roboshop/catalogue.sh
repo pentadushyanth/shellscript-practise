a538c64ef82b87044fa8b95b654e03985e04fa84
dushyanth <pentadushyanth@gmail.com>
2026-01-06 18:24:34 +0530

    first commit

---
+++ Common_roboshop/catalogue.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source ./common.sh
+server_name=catalogue
+check_root
+
+app_setup
+
+nodejs_setup
+
+service_enable
+
+
+cp $script_dir/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copying mongodb repo"
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "installing mongodb"
+
+Index=$(mongosh $mongodb_host --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [[ "$Index" -le 0 ]]; then
+    mongosh --host $mongodb_host </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "loading master data "
+else
+    echo -e "catalogue producsts already loaded $Y Skipping $N"
+fi
+
+app_restart
+
+print_total_time
--- Common_roboshop/common.sh
+++ Common_roboshop/common.sh
@@ -10,6 +10,8 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 Start_time=$(date +%s)
+mongodb_host=mongodb.practisedevops.shop
+
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
@@ -28,6 +30,68 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
     else
         echo -e " $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
+}
+
+nodejs_setup(){
+    dnf module disable nodejs -y &>>$LOG_FILE
+    VALIDATE $? " Disabled Node js"
+
+    dnf module enable nodejs:20 -y &>>$LOG_FILE
+    VALIDATE $? " enabled Node js"
+
+    dnf install nodejs -y &>>$LOG_FILE
+    VALIDATE $? " installed Node js"
+
+    npm install &>>$LOG_FILE
+    VALIDATE $? "installing dependencies"
+}
+
+service_enable(){
+    cp $script_dir/$server_name.service /etc/systemd/system/$server_name.service
+    VALIDATE $? "copying service"
+
+# sed -i 's/<MONGODB-SERVER-IPADDRESS>/mongodb.practisedevops.shop/g' /etc/systemd/system/catalogue.service
+
+    systemctl daemon-reload
+    VALIDATE $? "daemon reload"
+
+    systemctl enable $server_name &>>$LOG_FILE
+    VALIDATE $? "$server_name enabled"
+
+    systemctl start $server_name 
+    VALIDATE $? "$server_name started"
+}
+
+user_creation(){
+id roboshop
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "creating system user"
+else 
+    echo -e "user already exists...$Y SKIPPING $N"
+fi
+}
+
+app_setup(){
+   
+mkdir -p /app 
+VALIDATE $? "creating directory"
+
+curl -o /tmp/$server_name.zip https://roboshop-artifacts.s3.amazonaws.com/$server_name-v3.zip &>>$LOG_FILE
+VALIDATE $? "downloading $server_name"
+cd /app 
+VALIDATE $? "change directory"
+
+rm -rf /app/*
+VALIDATE $? "removing existing code"
+
+unzip /tmp/$server_name.zip &>>$LOG_FILE
+VALIDATE $? "unzipping file" 
+}
+
+app_restart(){
+systemctl restart $server_name
+VALIDATE $? "app restarted"
 }
 
 print_total_time(){
