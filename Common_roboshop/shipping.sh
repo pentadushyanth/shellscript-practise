e603f5e4450ce22fc2783e6e6d318c0e04a3823f
dushyanth <pentadushyanth@gmail.com>
2026-01-06 21:30:49 +0530

    first commit

--- Common_roboshop/shipping.sh
+++ Common_roboshop/shipping.sh
@@ -23,11 +23,12 @@ service_enable
 dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "mysql installed"
 
-mysql -h mysql.practisedevops.shop -uroot -pRoboShop@1 -e 'use mysql'
+mysql -h $Mysql_Host -uroot -pRoboShop@1 -e 'use mysql'
+
 if [ $? -ne 0 ]; then
-    mysql -h mysql.practisedevops.shop -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
-    mysql -h mysql.practisedevops.shop -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE  
-    mysql -h mysql.practisedevops.shop -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+    mysql -h $Mysql_Host -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h $Mysql_Host -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE  
+    mysql -h $Mysql_Host -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
 else
     echo -e "Shipping data is alreay loaded... $Y SKIPPING $N"
 fi
