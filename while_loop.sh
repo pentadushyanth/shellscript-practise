4ade1a89f407d45a215749187fdb655dba3779ff
dushyanth <pentadushyanth@gmail.com>
2026-01-08 11:41:07 +0530

    latest commit

--- while_loop.sh
+++ while_loop.sh
@@ -1,15 +1,20 @@
 #! /bin/bash
 
 # Initialize a counter variable
-counter=1
-max=5
+# counter=1
+# max=5
 
-# Loop while the counter is less than or equal to the maximum
-while [ $counter -le $max ]
-do
-  echo "Count is $counter"
-  # Increment the counter using C-style arithmetic
-  ((counter++))
-done
+# # Loop while the counter is less than or equal to the maximum
+# while [ $counter -le $max ]
+# do
+#   echo "Count is $counter"
+#   # Increment the counter using C-style arithmetic
+#   ((counter++))
+# done
 
-echo "Loop finished."
+# echo "Loop finished."
+
+ while IFS= read -r LINE; do
+        # Process the current line stored in the variable $LINE
+        echo "each line:$LINE"
+done < script20.sh
