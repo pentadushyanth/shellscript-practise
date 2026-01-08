e8c22e8c2b1743590eedab918e5a047865cfc62d
dushyanth <pentadushyanth@gmail.com>
2026-01-08 12:54:16 +0530

    latest commit

--- backup-script.sh
+++ backup-script.sh
@@ -6,7 +6,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
+source_dir=$1
+dest_dir=$2
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
@@ -27,3 +28,14 @@ Usage(){
 if [ $# -lt 2 ]; then
     Usage
 fi
+
+if [ ! -d $source_dir ]; then
+    echo -e "$R $source_dir does not exist $N"
+    exit 1
+fi
+
+if [ ! -d $dest_dir ]; then
+    echo -e "$R $dest_dir does not exist $N"
+    exit 1
+fi
+
