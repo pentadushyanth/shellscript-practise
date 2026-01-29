da9b78bcb7cccd4eba0ec65cd24a2e00d4e89969
pentadushyanth <pentadushyanth@gmail.com>
2026-01-29 20:06:53 +0530

    commit

--- cpu_usage.sh
+++ cpu_usage.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
 disk_usage=$(df -hT | grep -v Filesystem)  #to format th e table by removing the header row
-
+disk_threshold=2 # in projects we will keep it as 75
 while IFS= read -r line
 do 
-    echo "Line: $line"
-
+    usage=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    partition=$(echo $line | awk '{print $7}')
+    if [ $usage -ge $disk_threshold ]; then
+        echo " High usage on $partition: $usage"
+    fi
 done <<< $disk_usage 
