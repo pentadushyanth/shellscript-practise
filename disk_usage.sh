ec689d6ae19dd9c30a0cdb03726b6a8f5d3e547a
pentadushyanth <pentadushyanth@gmail.com>
2026-01-29 21:22:48 +0530

    commit

---
+++ disk_usage.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+
+
+disk_usage=$(df -hT | grep -v Filesystem)  #to format th e table by removing the header row
+disk_threshold=2 # in projects we will keep it as 75\
+Ipaddress= $(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+message=""
+while IFS= read -r line
+do 
+    usage=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    partition=$(echo $line | awk '{print $7}')
+    if [ $usage -ge $disk_threshold ]; then
+        message+="High disk usage $partition: $usage % \n" 
+    fi
+done <<< $disk_usage 
+
+echo -e "Message body: $message" 
+
+sh mail.sh "practisedevopsaws2025@gmail.com" "alert email" "high disk usage" "$message" "$Ipaddress" "devops team"
---
+++ mail.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+to_address=$1
+subject=$2
+alert_type=$3
+msg_body=$4
+Ipaddress=$5
+to_team=$6
+
+Final_body=$(sed -e "s/to_team/$to_team/g" -e "s/alert_type/$alert_type/g" -e "s/Ipaddress/$Ipaddress/g" -e "s/message_body/$msg_body/g" template.html)
+
+{
+echo "To: $to_address"
+echo "Subject: $subject"
+echo "Content-Type: text/html"
+echo ""
+echo "$Final_body"
+} | msmtp "$to_address"
