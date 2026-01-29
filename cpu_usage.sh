d3adfb340d06888866e0f6eae8f27d75089a4d20
pentadushyanth <pentadushyanth@gmail.com>
2026-01-29 20:00:25 +0530

    commit

---
+++ cpu_usage.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+disk_usage=$(df -hT | grep -v Filesystem)  #to format th e table by removing the header row
+
+While IFS= read -r line
+do 
+    echo "Line: $line"
+
+done <<< $disk_usage 
