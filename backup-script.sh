c1d69608643f3d9ae981a103c5d67a33376f65f1
pentadushyanth <pentadushyanth@gmail.com>
2026-01-29 18:50:22 +0530

    commit

--- backup-script.sh
+++ backup-script.sh
@@ -11,7 +11,8 @@ dest_dir=$2
 days=${3:-14}  #if not provided default to 14
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+#LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+LOG_FILE="$LOGS_FOLDER/backup.log" # modified to run the script as command
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
